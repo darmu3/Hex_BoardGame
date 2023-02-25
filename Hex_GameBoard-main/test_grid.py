ae5d1752c0ec7d491de8688d0b1d5b2bb09fb333
Александр Сунцов <117808824+darmu3@users.noreply.github.com>
2023-02-25 14:26:41 +0300

    Add files via upload

--- Hex_GameBoard-main/test_grid.py
+++ Hex_GameBoard-main/test_grid.py
@@ -14,12 +14,19 @@ class Example(QWidget):
         grid = QGridLayout()
         self.setLayout(grid)
 
-        names = ['1', '2', '3', '4', '5',
-        '6', '7', '8', '9', '10',
-        '11', '12', '13', '14', '15',
-        '16', '17', '18', '19', '20']
+        names = ['1', '2', '3', '4', '5','6', '7', '8', '9', '10','11',
+        '12', '13', '14', '15','16', '17', '18', '19', '20', '21', '22',
+        '23', '24','25','26','27', '28', '29', '30', '31','32', '33',
+        '34','35','36','37', '38', '39', '40', '41','42', '43','44',
+        '45','46','47', '48', '49', '50', '51','52', '53','54','55',
+        '56','57', '58', '59', '60', '61','62', '63','64','65','66',
+        '67', '68', '69', '70', '71','72', '73','74','75','76','77',
+        '78', '79', '80', '81','82', '83','84','85','86','87','88',
+        '89', '90', '91','92', '93','94','95','96','97','98','99',
+        '100', '101','102', '103','104','105','106','107','108','109','110',
+        '111','112', '113','114','115','116','117','118','119','120','121']
 
-        positions = [(i, j) for i in range(4) for j in range(5)]
+        positions = [(i, j) for i in range(11) for j in range(11)]
 
         for position, name in zip(positions, names):
          if name == '':
@@ -28,12 +35,12 @@ class Example(QWidget):
          grid.setSpacing(0)
          label = QLabel(self)
          label.setText(name)
-         label.setStyleSheet("min-width: 30px; min-height: 30px; border: 1px solid #444")
+         label.setStyleSheet("min-width: 20px; min-height: 20px; border: 1px solid #444")
          label.setAlignment(Qt.AlignCenter)
          # grid.addWidget(button, *position)
          grid.addWidget(label, *position)
 
-        self.move(300, 150)
+        self.move(350, 150)
         self.setWindowTitle('PyQt window')
         self.show()
 
